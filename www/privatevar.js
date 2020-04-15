2e263b680798bcbab103cabdd78622e4a02ce858
rgfpy <r2galeano@gmail.com>
2020-04-15 21:39:22 +0000

    ios test

--- www/privatevar.js
+++ www/privatevar.js
@@ -1,5 +1,5 @@
 module.exports = {
 	get: function (parmPost, successCallback, errorCallback) {
-		cordova.exec(successCallback, errorCallback, "PrivatevarPlugin", "get", [parmPost]);
+		cordova.exec(successCallback, errorCallback, "PrivateVar", "get", [parmPost]);
 	}
 };

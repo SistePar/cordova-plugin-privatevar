8a7cb4456870698e78f62d7c425769396d564478
rgfpy <r2galeano@gmail.com>
2020-04-15 22:38:32 +0000

    ios test

---
+++ src/ios/CDVPrivateVar.h
@@ -0,0 +1,11 @@
+/**
+ * CDVPrivateVar.h Cordova Plugin Header
+ */
+
+#import <Cordova/CDVPlugin.h>
+
+@interface CDVPrivateVar : CDVPlugin
+
+- (void)get:(CDVInvokedUrlCommand*)command;
+
+@end
---
+++ src/ios/CDVPrivateVar.m
@@ -0,0 +1,24 @@
+/**
+ * CDVPrivateVar.m Cordova Plugin Implementation
+ */
+
+#import "CDVPrivateVar.h"
+#import <Cordova/CDVPlugin.h>
+
+@implementation CDVPrivateVar
+
+- (void)get:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* get = [command.arguments objectAtIndex:0];
+
+    if (get != nil && [get length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:get];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end

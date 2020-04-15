1e5d16673db2a45c295342cb70d50f69dc897096
rgfpy <r2galeano@gmail.com>
2020-04-15 23:47:52 +0000

    ios test

--- www/privatevar.js
+++ www/privatevar.js
@@ -2,8 +2,8 @@
 var PrivateVar = function() {
 };
 
-PrivateVar.prototype.get = function() {
-	cordova.exec(function() {}, function() {}, "PrivateVar", "get", []);
+PrivateVar.prototype.get = function(getDatas) {
+	cordova.exec(getDatas, function() {}, "PrivateVar", "get", []);
 };
 
 module.exports = PrivateVar;

5a6a05281fec751294e4db1f7fb333bb69711b94
rgfpy <r2galeano@gmail.com>
2020-04-16 14:25:30 +0000

    fix

--- src/android/PrivateVar.java
+++ src/android/PrivateVar.java
@@ -6,7 +6,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Privatevar extends CordovaPlugin {
+public class PrivateVar extends CordovaPlugin {
     /**
      * Ejecutar request y response a PluginResult
      * @param action          The action to execute.

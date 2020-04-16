f1a21fa93c335a2751d68303c91c51f263f6f92d
rgfpy <r2galeano@gmail.com>
2020-04-16 01:14:38 +0000

    work fine in ios

--- www/privatevar.js
+++ www/privatevar.js
@@ -1,8 +1,6 @@
-var exec = require('cordova/exec');
-var cordova = require('cordova');
 
 module.exports = {
 	get: function(dataReceivedCallback) {
-		exec(dataReceivedCallback, null, "PrivateVar", "getDatas", []);
+		cordova.exec(dataReceivedCallback, null, "PrivateVar", "getDatas", []);
 	}
 };
